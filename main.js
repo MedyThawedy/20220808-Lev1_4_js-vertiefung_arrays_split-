2b5293819da304f438225903875be67a7abb204f
MD <dhmedy@googlemail.com>
2022-08-08 15:35:47 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// Deklariere Strings in, Variablen meinText1, meinText2, meinText3.
+// Definiere die Variable meinText1 mit dem Wert: Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag.
+// Definiere die Variable meinText2 mit dem Wert: Wie geht es dir heute?
+// Definiere die Variable meinText3 mit dem Wert: Heute ist ein großer Tag für uns.
+
+let meinText1 = 'Hallo Welt. Ich wünsche euch allen einen guten Arbeitstag.';
+let meinText2 = 'Wie geht es dir heute?';
+let meinText3 = 'Heute ist ein großer Tag für uns.';
+
+let split1, split2, split3;
+// Definiere die Variable split1 mit dem Wert: meinText1.split();
+split1 = meinText1.split();
+console.log(split1);
+// Definiere die Variable split2 mit dem Wert: meinText1.split("");
+split2 = meinText1.split("");
+console.log(split2);
+// Definiere die Variable split3 mit dem Wert: meinText1.split(" ");
+split3 = meinText1.split(" ");
+console.log(split3);
